a408671cec19bc3bbae8e4dd8758ad1aaf6b44b4
Mitsuharu Emoto <mthr1982@gmail.com>
2023-09-23 11:28:03 +0900

    Initial Commit

---
+++ AppIntentsSample/AppIntentsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIntentsSampleApp.swift
+//  AppIntentsSample
+//
+//  Created by Mitsuharu Emoto on 2023/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AppIntentsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
